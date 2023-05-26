d6f07c95d8d55e193105c46d647aae47a9e28819
h4med <hamed.d@gmail.com>
2023-05-26 10:19:56 +0330

    add ask me

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('write-code/', views.write_code, name='write_code'),
+    path('ask-me/', views.ask_me, name='ask_me'),
 ]
