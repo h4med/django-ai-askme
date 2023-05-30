1aa38fcc75466a5187450dc7620907312a45e57d
h4med <hamed.d@gmail.com>
2023-05-30 19:24:44 +0330

    created_at added to db

--- website/models.py
+++ website/models.py
@@ -5,6 +5,7 @@ class Dialogue(models.Model):
 	user = models.ForeignKey(User, related_name="code", on_delete=models.DO_NOTHING)
 	question = models.TextField(max_length=5000)
 	answer = models.TextField(max_length=5000)
+	created_at = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.question
